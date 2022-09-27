22ab51d5dd47d4f4e20563cd3d708b6180818f96
Ankit Kumar Mishra <ankit5kumar14@gmail.com>
2022-09-27 23:05:58 +0530

    Order Status Management: Cancel order implemented

--- JavaScript/Projects/OrderStatusManagement2/js/createOrderCard.js
+++ JavaScript/Projects/OrderStatusManagement2/js/createOrderCard.js
@@ -2,6 +2,8 @@ const createOrderCard = (orderId) => {
 	let colDiv = document.createElement('div')
 	let cardDiv = document.createElement('div')
 	let cardHeaderDiv = document.createElement('div')
+	let orderIdSpan = document.createElement('span')
+	let cancelButtonI = document.createElement('i')
 	let cardBodyDiv = document.createElement('div')
 	let cardTitleH5 = document.createElement('h5')
 	let cardTextP = document.createElement('p')
@@ -10,6 +12,7 @@ const createOrderCard = (orderId) => {
 
 	colDiv.classList = 'col-md-3'
 	cardDiv.classList = 'card text-center'
+	cancelButtonI.classList = 'fa-solid fa-xmark cancel-btn'
 	cardHeaderDiv.classList = 'card-header'
 	cardBodyDiv.classList = 'card-body'
 	cardTitleH5.classList = 'card-title'
@@ -17,16 +20,27 @@ const createOrderCard = (orderId) => {
 	orderStatusSpan.classList = 'badge rounded-pill bg-success'
 	cardFooterDiv.classList = 'card-footer'
 
-	cardHeaderDiv.innerHTML = `Order No.: <b>${orderId}</b>`
+	orderIdSpan.innerHTML = `Order No.: <b>${orderId}</b>`
 	cardTitleH5.innerText = `Bill Amount: $${Math.ceil(Math.random()*42) + 10 }`
 	cardTextP.innerHTML = `Large Pizza - ${Math.ceil(Math.random()*3)} Nos<br>Medium Pizza - ${Math.ceil(Math.random()*3)} Nos`
 	orderStatusSpan.innerText = 'Order placed'
 	orderStatusSpan.id = orderId
 	cardFooterDiv.innerText = '23-09-2022 09:47:36 PM'
 
+	cancelButtonI.addEventListener('click', () => {
+		colDiv.style.display = 'none'
+		document.getElementById('order-alert').style.display = 'block'
+
+		setTimeout(() => {
+			document.getElementById('order-alert').style.display = 'none'
+		}, 5000)
+	})
+
 	cardBodyDiv.appendChild(cardTitleH5)
 	cardBodyDiv.appendChild(cardTextP)
 	cardBodyDiv.appendChild(orderStatusSpan)
+	cardHeaderDiv.appendChild(orderIdSpan)
+	cardHeaderDiv.appendChild(cancelButtonI)
 	cardDiv.appendChild(cardHeaderDiv)
 	cardDiv.appendChild(cardBodyDiv)
 	cardDiv.appendChild(cardFooterDiv)
@@ -39,6 +53,7 @@ const createOrderCard = (orderId) => {
 		<div class="card text-center">
 			<div class="card-header">
 				Order No.: MP202209230005
+				<i class="fa-solid fa-xmark"></i>
 			</div>
 			<div class="card-body">
 				<h5 class="card-title">Bill Amount: $52</h5>
